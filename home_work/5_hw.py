85387b5730847699da0aebf0a1f12baacb923ad6
olgapotter <pink1234@mail.ru>
2025-04-29 16:25:56 +0300

    First Comit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,34 +1,34 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-# from selenium.common.exceptions import NoSuchElementException
-# import time
-#
-# def find_elements_on_saucedemo():
-#
-#     options = Options()
-#     options.add_argument("--headless")
-#     service = Service()
-#     driver = webdriver.Chrome(service=service, options=options)
-#
-#     try:
-#         driver.get("https://www.saucedemo.com/")
-#         time.sleep(2)
-#
-#         username = driver.find_element(By.ID, "user-name")
-#         password = driver.find_element(By.ID, "password")
-#         submit_button = driver.find_element(By.ID, "login-button")
-#
-#         if username and password and submit_button:
-#             print("Элементы найдены")
-#
-#     except NoSuchElementException:
-#         print("Один или несколько элементов не найдены")
-#
-#     finally:
-#         driver.quit()
-#
-# find_elements_on_saucedemo()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+def find_elements_on_saucedemo():
+
+    options = Options()
+    options.add_argument("--headless")
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        driver.get("https://www.saucedemo.com/")
+        time.sleep(2)
+
+        username = driver.find_element(By.ID, "user-name")
+        password = driver.find_element(By.ID, "password")
+        submit_button = driver.find_element(By.ID, "login-button")
+
+        if username and password and submit_button:
+            print("Элементы найдены")
+
+    except NoSuchElementException:
+        print("Один или несколько элементов не найдены")
+
+    finally:
+        driver.quit()
+
+find_elements_on_saucedemo()
 
 
